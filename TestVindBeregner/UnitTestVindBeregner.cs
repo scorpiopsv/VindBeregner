feec98123a9873eac4901f3d293c05cb9b7b03f5
Philip Holm Svendsen <psv@plass.dk>
2020-05-04 11:22:44 +0200

    psv

--- TestVindBeregner/UnitTestVindBeregner.cs
+++ TestVindBeregner/UnitTestVindBeregner.cs
@@ -28,5 +28,35 @@ namespace TestVindBeregner
 
             Xunit.Assert.Equal(0, effekt);
         }
+
+        [TestMethod]
+        public void TestVindstyrkeVed14_5ms()
+        {
+            VindMoellerBeregner p = new VindMoellerBeregner();
+
+            double effekt = p.MegaWatt(14.5, 500);
+
+            Xunit.Assert.Equal(250, effekt);
+        }
+
+        [TestMethod]
+        public void TestVindstyrkeOverMax()
+        {
+            VindMoellerBeregner p = new VindMoellerBeregner();
+
+            double effekt = p.MegaWatt(30, 500);
+
+            Xunit.Assert.Equal(0, effekt);
+        }
+
+        [TestMethod]
+        public void TestVindstyrkeUnderMin()
+        {
+            VindMoellerBeregner p = new VindMoellerBeregner();
+
+            double effekt = p.MegaWatt(2, 500);
+
+            Xunit.Assert.Equal(0, effekt);
+        }
     }
 }
